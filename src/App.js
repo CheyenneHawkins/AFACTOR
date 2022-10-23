d02ed467d8bea0e10529c02c3086f2153159a658
Cheyenne Hawkins <cheyenne.hawkins@gmail.com>
2022-10-23 17:54:39 -0500

    adds basic header layout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from "./images/AF_LOGO.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header >
+        <div className="header">
+          <div className="header-logo">
+            <img src={logo} className="header-logo" alt="AFACTOR Media" />
+          </div>
+          <div className=""></div>
+          <div className="header-menus">
+            <div className="menu-links">
+              <div className="menu-item">About</div>
+              <div className="menu-item">Work</div>
+              <div className="menu-item">Contact</div>
+              <div className="menu-item">Client Login</div>
+
+            </div>
+
+          </div>
+        </div>
+        <div className="contentmain">
+          Meat
+
+        </div>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { render } from '@testing-library/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
@@ -15,3 +17,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
